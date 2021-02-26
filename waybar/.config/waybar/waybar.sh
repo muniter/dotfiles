1e241a58239ba1112578737abb6f5979705fe564
Javier López <graulopezjavier@gmail.com>
2021-02-25 21:45:16 -0500

    feat: Waybar has tray support now

--- waybar/.config/waybar/waybar.sh
+++ waybar/.config/waybar/waybar.sh
@@ -2,9 +2,12 @@
 
 # Terminate already running bar instances
 killall -q waybar
+pkill -f waybar-data-getter.sh
 
 # Wait until the processes have been shut down
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
+# Data getter
+# ./waybar-data-getter.sh
 # Launch main
 waybar
